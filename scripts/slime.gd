6d39d5e98fc2916b9683b8f6766069467a62c539
Balint000 <erdeibalint01@gmail.com>
2026-02-04 18:10:49 +0100

    knight facing and bg music

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -6,10 +6,13 @@ var direction = 1
 
 @onready var ray_cast_rigth: RayCast2D = $RayCastRigth
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(delta: float) -> void:
 	if ray_cast_rigth.is_colliding():
 		direction = -1
+		animated_sprite_2d.flip_h = true
 	if ray_cast_left.is_colliding():
 		direction = 1
+		animated_sprite_2d.flip_h = false
 	position.x += direction * SPEED * delta
